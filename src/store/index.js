24dd5c347e4451145f36ce822f211236ba9c4d12
Hsin-ju Yen <hjyen@umich.edu>
2018-01-29 17:26:33 -0500

    show latest email threads on the sidebar

--- src/store/index.js
+++ src/store/index.js
@@ -14,12 +14,21 @@ const store: Data = require('./emails.json');
  * deeply nested attributes of Messages or Threads...
  */
 
+
 // For example, this returns the subject of a given message if it is defined
 // by a header in the message payload
-export function getMessageSubject(message: Message): ?string {
+export function getMessageInfo(message: Message, headerName: string): ?string {
   const { headers } = message.payload;
-  const subjectHeader = headers.find(header => header.name === 'Subject');
-  return subjectHeader && subjectHeader.value;
+  const headerInfo = headers.find(header => header.name === headerName);
+  return headerInfo && headerInfo.value;
 }
+
+export function changeTimeFormat(time: string): string {
+  const date = new Date(time).toLocaleDateString();
+  const hour = new Date(time).toLocaleTimeString();
+  const result = `${hour} , ${date}`;
+  return result;
+}
+
 
 export default store;
